e1ea1c96db84c8b442fe29927b2f9ed02ea1f7bb
Stéphane Bonhomme <stephane@exselt.com>
2017-02-24 02:27:17 +0100

    docker sources

---
+++ entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+sleep 10
+set -e
+
+if [ -z "$UID" ] ; then
+  UID=0
+fi
+
+PYTHONPATH=/kolekti/src/kolekti_server:/kolekti/src celery worker -A kolekti_server.celeryconf -Q default -n default@${UID} --uid ${UID}  --loglevel=DEBUG 
