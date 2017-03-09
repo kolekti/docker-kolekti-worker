bb2ef3131aa1237ee023f09a761741ad0388f1a0
Stéphane Bonhomme <stephane@exselt.com>
2017-03-09 01:19:11 +0100

    set loglevel to INFO

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ if [ -z "$UID" ] ; then
   UID=0
 fi
 
-PYTHONPATH=/kolekti/src/kolekti_server:/kolekti/src celery worker -A kolekti_server.celeryconf -Q default -n default@${UID} --uid ${UID}  --loglevel=DEBUG 
+PYTHONPATH=/kolekti/src/kolekti_server:/kolekti/src celery worker -A kolekti_server.celeryconf -Q default -n default@${UID} --uid ${UID}  --loglevel=INFO 
