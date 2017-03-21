9d6f87cd6fb28122a1ff85ce4f224ba2c2712f32
Stéphane Bonhomme <stephane@exselt.com>
2017-03-21 16:51:42 +0100

    sets log to DEBUG if env KOLEKTI_DEBUG is True

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,7 @@ if [ -z "$UID" ] ; then
   UID=0
 fi
 
-PYTHONPATH=/kolekti/src/kolekti_server:/kolekti/src celery worker -A kolekti_server.celeryconf -Q default -n default@${UID} --uid ${UID}  --loglevel=INFO 
+LOG_LEVEL="INFO"
+[ "$KOLEKTI_DEBUG" = "True" ] && LOG_LEVEL="DEBUG"
+
+PYTHONPATH=/kolekti/src/kolekti_server:/kolekti/src celery worker -A kolekti_server.celeryconf -Q default -n default@${UID} --uid ${UID}  --loglevel=${LOG_LEVEL}
