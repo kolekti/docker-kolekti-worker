65f33286d659402ca88c16b1a394ccdfa2e0f5d5
Stéphane Bonhomme <stephane@exselt.com>
2017-05-15 17:04:47 +0200

    adds user creation at startup

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,4 +9,6 @@ fi
 LOG_LEVEL="INFO"
 [ "$KOLEKTI_DEBUG" = "True" ] && LOG_LEVEL="DEBUG"
 
+getent passwd ${UID} || useradd -u ${UID} -M kolekti
+
 PYTHONPATH=/kolekti/src/kolekti_server:/kolekti/src celery worker -A kolekti_server.celeryconf -Q default -n default@${UID} --uid ${UID}  --loglevel=${LOG_LEVEL}
